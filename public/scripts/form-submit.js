456825d865d3ce5754b187e34d44f24bbd993e4e
Gozi1 <snwabiani@gmail.com>
2022-11-18 15:27:36 +0000

    add form validation

--- public/scripts/form-submit.js
+++ public/scripts/form-submit.js
@@ -1,19 +1,29 @@
 //Ajax form submit 
 
 $(document).ready(function() {
+  const validator = (data) => {
+    truedata = data.split("=")[1];
+    if(!truedata ||truedata === " ")return "tweet cannot be empty";
+    else if(truedata.length >140)return "tweet is above maximum length" ;
+    return null;
+  }
   $( "#tweet-form" ).submit(function( event ) {
     event.preventDefault();
-    // console.log( $( this ).serialize() );
-    const form = $(this);
-    const actionUrl = form.attr('action');
-    
+    // serializes the form's elements.
+    const form = $(this).serialize();
+    const actionUrl = $(this).attr('action');
+    const validate = validator(form)
+    if(validate){
+      alert(validate);
+      return false
+    }
     $.ajax({
         type: "POST",
         url: actionUrl,
-        data: form.serialize(), // serializes the form's elements.
+        data: form, 
         success: function(data)
         {
-          alert(data); // show response from the php script.
+          console.log("post success"); // show response from the php script.
         }
     });
   });
